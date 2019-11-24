9dcd724dfcc3e93371ecd207c0fec1f09b192596
codewithsrobins1 <codewithsrobins1@gmail.com>
2019-11-23 16:28:44 -0800

    112319

---
+++ Background Color Change/script.js
@@ -0,0 +1,17 @@
+//Background Color Change
+
+//Target the Button
+//Target the body
+//Add Event Listener to Button When Clicked
+//Change the Background Color from an array
+
+const mainBody = document.querySelector('body');
+const colorBtn = document.getElementById('buttonTest');
+
+const colors = ['yellow', 'green', 'red', 'dodgerblue', 'white'];
+
+function changeColor(){
+  
+}
+
+colorBtn.addEventListener('click', changeColor);
