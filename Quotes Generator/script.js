84fcbf8296101e94e0fcc672c20bf9817d782835
codewithsrobins1 <codewithsrobins1@gmail.com>
2019-11-24 23:45:28 -0800

    112419

---
+++ Quotes Generator/script.js
@@ -0,0 +1,24 @@
+const quoteBtn = document.getElementById('quoteBtn');
+const quoteDisplay = document.getElementById('quote')
+const quoteAuth = document.getElementsByClassName('person')
+
+const simpleQuotes = [
+  {
+    name: 'Gandhi',
+    quote: 'Be the Change...'
+  },
+  {
+    name: 'Kawhi',
+    quote: 'What it do baby'
+  },
+  {
+    name: 'Caesar',
+    quote: 'Et tu Brute'
+  }
+];
+
+//Change id Quote to Quote from the SimpleQuotes Array
+
+//Change Person to the Author of the Quote
+
+//Create Event Listener for the Button
